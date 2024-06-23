4ec75a4bbe6b65fe393193b65692d229c883f8a6
Nicholas Fiorentini <lordgordon@users.noreply.github.com>
2024-06-23 22:25:57 +0200

    completed (03) ticket_v1 - (12) outro

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -5,7 +5,78 @@
 //   The unit price is in cents and must be strictly greater than zero.
 //   Order must include a method named `total` that returns the total price of the order.
 //   Order must provide setters and getters for each field.
-//
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        Self::validate_product_name(&product_name);
+        Self::validate_quantity(quantity);
+        Self::validate_unit_price(unit_price);
+
+        Self {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    fn validate_product_name(product_name: &String) {
+        if product_name.is_empty() {
+            panic!("Product name must not be empty")
+        }
+        if product_name.len() > 300 {
+            panic!("Product name is longer than 300 bytes")
+        }
+    }
+
+    fn validate_quantity(quantity: u32) {
+        if quantity <= 0 {
+            panic!("Quantity must be greater than 0")
+        }
+    }
+
+    fn validate_unit_price(unit_price: u32) {
+        if unit_price <= 0 {
+            panic!("Unit price must be greater than 0")
+        }
+    }
+
+    pub fn product_name(&self) -> &str {
+        self.product_name.as_str()
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, new_product_name: String) {
+        Self::validate_product_name(&new_product_name);
+        self.product_name = new_product_name;
+    }
+
+    pub fn set_quantity(&mut self, new_quantity: u32) {
+        Self::validate_quantity(new_quantity);
+        self.quantity = new_quantity;
+    }
+
+    pub fn set_unit_price(&mut self, new_unit_price: u32) {
+        Self::validate_unit_price(new_unit_price);
+        self.unit_price = new_unit_price;
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+}
+
 // Tests are located in a different place this time—in the `tests` folder.
 // The `tests` folder is a special location for `cargo`. It's where it looks for **integration tests**.
 // Integration here has a very specific meaning: they test **the public API** of your project.
