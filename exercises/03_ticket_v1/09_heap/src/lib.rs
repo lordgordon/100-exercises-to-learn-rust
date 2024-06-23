50344f913df3f55b236eb6124ea8f547669d088d
Nicholas Fiorentini <lordgordon@users.noreply.github.com>
2024-06-23 22:04:48 +0200

    completed 03) ticket_v1 - (09) heap

--- exercises/03_ticket_v1/09_heap/src/lib.rs
+++ exercises/03_ticket_v1/09_heap/src/lib.rs
@@ -4,8 +4,6 @@ pub struct Ticket {
     status: String,
 }
 
-// TODO: based on what you learned in this section, replace `todo!()` with
-//  the correct **stack size** for the respective type.
 #[cfg(test)]
 mod tests {
     use super::Ticket;
@@ -13,7 +11,11 @@ mod tests {
 
     #[test]
     fn string_size() {
-        assert_eq!(size_of::<String>(), todo!());
+        assert_eq!(size_of::<String>(), 24);
+        // usize a 64bit arch, so u64 ->    8 bytes
+        // len is u64 ->                    8 bytes
+        // capacity is u64 ->               8 bytes
+        // Total                            24 bytes
     }
 
     #[test]
@@ -23,6 +25,7 @@ mod tests {
         // but, in general, the memory layout of structs is a more complex topic.
         // If you're curious, check out the "Data layout" section of the Rustonomicon
         // https://doc.rust-lang.org/nomicon/data.html for more information.
-        assert_eq!(size_of::<Ticket>(), todo!());
+        assert_eq!(size_of::<Ticket>(), 72);
+        // string * 3 = 24 * 3 = 72 bytes
     }
 }
