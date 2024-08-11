88ae0bb6fb3b333a8c77340c603ad56e42fc3745
Nicholas Fiorentini <lordgordon@users.noreply.github.com>
2024-08-11 21:25:53 +0200

    #: 07_threads/05_channels implementation

--- exercises/07_threads/05_channels/src/lib.rs
+++ exercises/07_threads/05_channels/src/lib.rs
@@ -1,13 +1,15 @@
 use std::sync::mpsc::{Receiver, Sender};
+use crate::data::TicketDraft;
+use crate::store::TicketStore;
 
 pub mod data;
 pub mod store;
 
 pub enum Command {
-    Insert(todo!()),
+    Insert(TicketDraft),
 }
 
-// Start the system by spawning the server the thread.
+// Start the system by spawning the server thread.
 // It returns a `Sender` instance which can then be used
 // by one or more clients to interact with the server.
 pub fn launch() -> Sender<Command> {
@@ -16,8 +18,21 @@ pub fn launch() -> Sender<Command> {
     sender
 }
 
-// TODO: The server task should **never** stop.
-//  Enter a loop: wait for a command to show up in
-//  the channel, then execute it, then start waiting
-//  for the next command.
-pub fn server(receiver: Receiver<Command>) {}
+pub fn server(receiver: Receiver<Command>) {
+    let mut db = TicketStore::new();
+
+    loop {
+        let message = receiver.recv();
+        match message {
+            Ok(command) => {
+                print!("Received command");
+                match command {
+                    Command::Insert(ticket) => {
+                        db.add_ticket(ticket);
+                    },
+                }
+            },
+            _ => panic!("Error"),
+        }
+    }
+}
