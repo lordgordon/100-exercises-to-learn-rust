dd2242d04a85edb783d87f60c61af68853d0f98e
Nicholas Fiorentini <lordgordon@users.noreply.github.com>
2024-08-13 17:16:39 +0200

    (08) futures - (04) future

--- exercises/08_futures/04_future/src/lib.rs
+++ exercises/08_futures/04_future/src/lib.rs
@@ -1,4 +1,4 @@
-//! TODO: get the code to compile by **re-ordering** the statements
+//! get the code to compile by **re-ordering** the statements
 //!  in the `example` function. You're not allowed to change the
 //!  `spawner` function nor what each line does in `example`.
 //!   You can wrap existing statements in blocks `{}` if needed.
@@ -10,7 +10,9 @@ fn spawner() {
 }
 
 async fn example() {
-    let non_send = Rc::new(1);
+    {
+        let non_send = Rc::new(1);
+        println!("{}", non_send);
+    }
     yield_now().await;
-    println!("{}", non_send);
 }
